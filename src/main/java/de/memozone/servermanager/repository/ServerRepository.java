e42cdb96fd4d0dd26272c4e491008e46e28712a6
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-01-09 19:25:23 +0100

    Initial commit

---
+++ src/main/java/de/memozone/servermanager/repository/ServerRepository.java
@@ -0,0 +1,10 @@
+package de.memozone.servermanager.repository;
+
+import de.memozone.servermanager.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+
+    Server findByIpAddress(String ipAddress);
+
+}
